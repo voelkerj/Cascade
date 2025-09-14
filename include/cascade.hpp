e160989a858a76396e627fdc1ec1db693defde3e
John Voelker <john.w.voelker@gmail.com>
2025-09-14 16:34:50 -0400

    basic animation system

--- include/cascade.hpp
+++ include/cascade.hpp
@@ -6,19 +6,52 @@
 #include "inputs.hpp"
 
 #include "../external/SDL/include/SDL3/SDL.h"
+#include "../external/SDL_image/include/SDL3_image/SDL_image.h"
+
+struct Camera {
+  // X & Y Position of Camera's origin point (center of screen)
+  float pos[2];
+
+  float zoom{1}; // Camera zoom factor. Ratio of pixels per world unit.
+
+  // Width & Height of the Camera's projected FOV in WCS
+  float FOV[2];
+
+  float screen_top_y;
+  float screen_bottom_y;
+  float screen_left_x;
+  float screen_right_x;
+};
+
+// Shared, read-only animation definition
+struct Animation {
+  int update_interval; // milliseconds
+  std::vector<SDL_FRect> frames;
+  std::string sprite_sheet;
+};
+
+// Per-entity animation state
+struct AnimationInstance {
+  std::string animation_name; // ID to look up the AnimationData
+  int frame_idx{0};
+  Uint32 prev_update_ticks{0};
+};
 
 class Cascade
 {
 public:
   Cascade();
-  ~Cascade(){};
+  ~Cascade();
 
   entt::entity CreateEntity();
   void DestroyEntity(entt::entity entity);
 
   template <typename T> void AddComponent(entt::entity entity, T component_data);
-
   template <typename T> void RemoveComponent(entt::entity entity);
+
+  void LoadSpriteSheet(std::string sheet_name, std::string sheet_path);
+  void CreateAnimation(std::string animation_name, std::string sheet_name, int update_interval);
+  void AddFrame(std::string animation_name, int x, int y, int w, int h);
 
   void StartFrame();
   void EndFrame();
@@ -32,9 +65,18 @@ private:
 
   // SDL Stuff
   std::string m_base_path;
+  SDL_Window* m_window;
+  int m_window_size[2]; // Width & Height
+  SDL_Renderer* m_renderer;
+
+  // Graphics
+  Camera m_camera;
+  float m_scale[2]; // X & Y scale
+  std::map<std::string, SDL_Texture*> m_sprite_sheets;
+  std::unordered_map<std::string, Animation> m_animations;
 
   // Frames
-  int fps{60};
+  int m_fps{60};
   Uint32 m_frame_start_ticks;
   Uint32 m_frame_end_ticks;
   void EnforceFPS();
--- src/cascade.cpp
+++ src/cascade.cpp
@@ -1,9 +1,45 @@
+#include <iostream>
+
 #include "cascade.hpp"
 
 Cascade::Cascade()
 {
+  // Initialize SDL
   SDL_Init(SDL_INIT_VIDEO);
   m_base_path = SDL_GetBasePath();
+
+  // Initialize Window
+  m_window = SDL_CreateWindow("Cascade", 0, 0, SDL_WINDOW_FULLSCREEN);
+  if (m_window == nullptr) {
+    std::cout << SDL_GetError() << std::endl;
+    exit(1);
+  }
+
+  // Initialize Renderer
+  m_renderer = SDL_CreateRenderer(m_window, NULL);
+  if (m_renderer == nullptr) {
+    std::cout << SDL_GetError() << std::endl;
+    exit(1);
+  }
+
+  SDL_SetRenderDrawColor(m_renderer, 0x26, 0x26, 0x26, 0xFF);
+
+  // Initialize Camera
+  SDL_GetWindowSize(m_window, &m_window_size[0], &m_window_size[1]);
+  int screen_width, screen_height;
+  SDL_GetCurrentRenderOutputSize(m_renderer, &screen_width, &screen_height);
+
+  m_camera.pos[0] = 0;
+  m_camera.pos[1] = 0;
+  m_camera.FOV[0] = screen_width / m_camera.zoom;
+  m_camera.FOV[1] = screen_height / m_camera.zoom;
+
+  m_scale[0] = m_window_size[0] / m_camera.FOV[0];
+  m_scale[1] = m_window_size[1] / m_camera.FOV[1];
+}
+
+Cascade::~Cascade() {
+  SDL_DestroyWindow(m_window);
 }
 
 entt::entity Cascade::CreateEntity()
@@ -24,6 +60,40 @@ template <typename T> void Cascade::AddComponent(entt::entity entity, T componen
 template <typename T> void Cascade::RemoveComponent(entt::entity entity)
 {
   m_entt_registry.remove<T>(entity);
+}
+
+void Cascade::LoadSpriteSheet(std::string sheet_name, std::string sheet_path)
+{
+  SDL_Texture* sprite_sheet = IMG_LoadTexture(m_renderer, sheet_path.c_str());
+
+  if (!sprite_sheet) {
+    std::cout << "Failed to load file " << sheet_path << "\n";
+    exit(1);
+  }
+
+  SDL_SetTextureScaleMode(sprite_sheet, SDL_SCALEMODE_NEAREST);  // use nearest pixel scaling
+
+  m_sprite_sheets.emplace(sheet_name, sprite_sheet);
+}
+
+void Cascade::CreateAnimation(std::string animation_name, std::string sheet_name, int update_interval)
+{
+  Animation new_animation;
+  new_animation.sprite_sheet = sheet_name;
+  new_animation.update_interval = update_interval;
+
+  m_animations.emplace(animation_name, new_animation);
+}
+
+void Cascade::AddFrame(std::string animation_name, int x, int y, int w, int h)
+{
+  SDL_FRect frame;
+  frame.x = x;
+  frame.y = y;
+  frame.w = w;
+  frame.h = h;
+
+  m_animations[animation_name].frames.push_back(frame);
 }
 
 void Cascade::StartFrame()
@@ -47,9 +117,9 @@ void Cascade::EnforceFPS()
 
   float elapsed_time = (m_frame_end_ticks - m_frame_start_ticks) / 1000;
 
-  if (elapsed_time < (1.0 / fps)) {
-    SDL_Delay((1.0 / fps) - elapsed_time);
-    elapsed_time = 1.0 / fps;
+  if (elapsed_time < (1.0 / m_fps)) {
+    SDL_Delay((1.0 / m_fps) - elapsed_time);
+    elapsed_time = 1.0 / m_fps;
   }
 }
 
